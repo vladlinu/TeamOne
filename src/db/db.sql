fd040251e7768a2847875b9ddd03508db45df6de
Vladlinu <echo@DESKTOP-0S828B7.localdomain>
2020-11-16 18:27:01 +0300

    Commands for groups table

---
+++ src/db/db.sql
@@ -0,0 +1,5 @@
+create table groups(id bigint primary key, name varchar(50) not null);
+
+insert into groups(id, name) values(1, 'First Group');
+insert into groups(id, name) values(2, 'Second Group');
+insert into groups(id, name) values(3, 'Third Group');
