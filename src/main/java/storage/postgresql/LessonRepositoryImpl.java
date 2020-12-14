56981b85fb3cc8fa28559c83f8de5f1819704417
Vladyslav <54894036+Proxima-C@users.noreply.github.com>
2020-12-14 20:28:56 +0200

    Add files via upload

--- src/main/java/storage/postgresql/LessonRepositoryImpl.java
+++ src/main/java/storage/postgresql/LessonRepositoryImpl.java
@@ -11,232 +11,292 @@ import java.time.LocalTime;
 import java.util.*;
 
 public class LessonRepositoryImpl implements LessonRepository {
-    private Connector connector;
+	private Connector connector;
 
-    public LessonRepositoryImpl(Connector connector) {
-        this.connector = connector;
-    }
+	public LessonRepositoryImpl(Connector connector) {
+		this.connector = connector;
+	}
 
-    @Override
-    public List<Lesson> findLessonsByGroupIdFromInterval(String groupId, LocalDate from, LocalDate to) {
-        String getLessonsCommand = String.format("SELECT * FROM Lessons WHERE date >= '%s' AND date < '%s' AND group_id = %s",
-                from, to, groupId);
+	@Override
+	public List<Lesson> findLessonsByGroupIdFromInterval(String groupId, LocalDate from, LocalDate to) {
+		String getLessonsCommand = String.format("SELECT * FROM Lessons WHERE date >= '%s' AND date < '%s' AND group_id = %s",
+				from, to, groupId);
 
-        ResultSet lessonsSet = connector.executeStatement(getLessonsCommand);
+		ResultSet lessonsSet = connector.executeStatement(getLessonsCommand);
 
-        ArrayList<Lesson> lessons = new ArrayList<>();
+		ArrayList<Lesson> lessons = new ArrayList<>();
 
-        try {
-            while (lessonsSet.next()) {
-                Integer id = lessonsSet.getInt(0);
-                LocalDate localDate = lessonsSet.getObject(1, LocalDate.class);
-                LocalTime localTime = lessonsSet.getObject(2, LocalTime.class);
+		try {
+			while (lessonsSet.next()) {
+				Integer id = lessonsSet.getInt(0);
+				LocalDate localDate = lessonsSet.getObject(1, LocalDate.class);
+				LocalTime localTime = lessonsSet.getObject(2, LocalTime.class);
 
-                LocalDateTime dateTime = localDate.atTime(localTime);
+				LocalDateTime dateTime = localDate.atTime(localTime);
 
-                String homework = lessonsSet.getString(3);
-                String discipline = lessonsSet.getString(4);
-                String description = lessonsSet.getString(6);
-                String teacherLogin = lessonsSet.getString(7);
+				String homework = lessonsSet.getString(3);
+				String discipline = lessonsSet.getString(4);
+				String description = lessonsSet.getString(6);
+				String teacherLogin = lessonsSet.getString(7);
 
-                Map<String, Boolean> presentsForLesson = new HashMap<>();
-                Lesson lesson = new Lesson(id, dateTime, description, discipline, homework, Integer.valueOf(groupId), teacherLogin, presentsForLesson);
+				Map<String, Boolean> presentsForLesson = new HashMap<>();
+				Lesson lesson = new Lesson(presentsForLesson);
 
-                String getPresentsCommand = String.format("Select * FROM Presents WHERE lesson_id = %s", id);
+				String getPresentsCommand = String.format("Select * FROM Presents WHERE lesson_id = %s", id);
 
-                ResultSet presentSet = connector.executeStatement(getPresentsCommand);
-                while (presentSet.next()) {
-                    lesson.addPresent(presentSet.getString(1));
-                }
-                lessons.add(lesson);
-            }
+				ResultSet presentSet = connector.executeStatement(getPresentsCommand);
+				while (presentSet.next()) {
+					lesson.addPresent(presentSet.getString(1));
+				}
 
-            lessonsSet.close();
+				lesson.setLessonId(id);
+				lesson.setDateTime(dateTime);
+				lesson.setDescription(description);
+				lesson.setHomework(homework);
+				lesson.setDiscipline(discipline);
+				lesson.setTeacherLogin(teacherLogin);
+				lesson.setGroupId(Integer.valueOf(groupId));
 
-            return lessons;
-        } catch (SQLException ex) {
-            return null;
-        }
-    }
+				lessons.add(lesson);
+			}
 
-    @Override
-    public List<Lesson> findLessonsByTeacherIdFromInterval(String teacherLogin, LocalDate from, LocalDate to) {
-        String getLessonsCommand = String.format("SELECT * FROM Lessons WHERE date >= '%s' AND date < '%s' AND teacher_login = '%s'",
-                from, to, teacherLogin);
+			lessonsSet.close();
 
-        ResultSet lessonsSet = connector.executeStatement(getLessonsCommand);
+			return lessons;
+		} catch (SQLException ex) {
+			return null;
+		}
+	}
 
-        ArrayList<Lesson> lessons = new ArrayList<>();
+	@Override
+	public List<Lesson> findLessonsByTeacherIdFromInterval(String teacherLogin, LocalDate from, LocalDate to) {
+		String getLessonsCommand = String.format("SELECT * FROM Lessons WHERE date >= '%s' AND date < '%s' AND teacher_login = '%s'",
+				from, to, teacherLogin);
 
-        try {
-            while (lessonsSet.next()) {
-                Integer id = lessonsSet.getInt(0);
-                LocalDate localDate = lessonsSet.getObject(1, LocalDate.class);
-                LocalTime localTime = lessonsSet.getObject(2, LocalTime.class);
+		ResultSet lessonsSet = connector.executeStatement(getLessonsCommand);
 
-                LocalDateTime dateTime = localDate.atTime(localTime);
+		ArrayList<Lesson> lessons = new ArrayList<>();
 
-                String homework = lessonsSet.getString(3);
-                String discipline = lessonsSet.getString(4);
-                Integer groupId = lessonsSet.getInt(5);
-                String description = lessonsSet.getString(6);
+		try {
+			while (lessonsSet.next()) {
+				Integer id = lessonsSet.getInt(0);
+				LocalDate localDate = lessonsSet.getObject(1, LocalDate.class);
+				LocalTime localTime = lessonsSet.getObject(2, LocalTime.class);
+
+				LocalDateTime dateTime = localDate.atTime(localTime);
+
+				String homework = lessonsSet.getString(3);
+				String discipline = lessonsSet.getString(4);
+				Integer groupId = lessonsSet.getInt(5);
+				String description = lessonsSet.getString(6);
 
 
-                Map<String, Boolean> presentsForLesson = new HashMap<>();
-                Lesson lesson = new Lesson(id, dateTime, description, discipline, homework, groupId, teacherLogin, presentsForLesson);
+				Map<String, Boolean> presentsForLesson = new HashMap<>();
+				Lesson lesson = new Lesson(presentsForLesson);
 
-                String getPresentsCommand = String.format("Select * FROM Presents WHERE lesson_id = %s", id);
+				String getPresentsCommand = String.format("Select * FROM Presents WHERE lesson_id = %s", id);
 
-                ResultSet presentSet = connector.executeStatement(getPresentsCommand);
-                while (presentSet.next()) {
-                    lesson.addPresent(presentSet.getString(1));
-                }
+				ResultSet presentSet = connector.executeStatement(getPresentsCommand);
+				while (presentSet.next()) {
+					lesson.addPresent(presentSet.getString(1));
+				}
 
-                lesson.setLessonId(id);
-                lesson.setDateTime(dateTime);
-                lesson.setDescription(description);
-                lesson.setHomework(homework);
-                lesson.setDiscipline(discipline);
-                lesson.setTeacherLogin(teacherLogin);
-                lesson.setGroupId(groupId);
+				lesson.setLessonId(id);
+				lesson.setDateTime(dateTime);
+				lesson.setDescription(description);
+				lesson.setHomework(homework);
+				lesson.setDiscipline(discipline);
+				lesson.setTeacherLogin(teacherLogin);
+				lesson.setGroupId(groupId);
 
-                lessons.add(lesson);
-            }
+				lessons.add(lesson);
+			}
 
-            lessonsSet.close();
+			lessonsSet.close();
 
-            return lessons;
-        } catch (SQLException ex) {
-            return null;
-        }
-    }
+			return lessons;
+		} catch (SQLException ex) {
+			return null;
+		}
+	}
 
-    @Override
-    public Lesson saveNewEntity(Lesson entity) {
-        return null;
-    }
+	@Override
+	public Lesson saveNewEntity(Lesson entity) {
+		String statement = "INSERT INTO Lessons VALUES('" + entity.getLessonId() +
+				"', '" + entity.getDateTime() + "', '" + entity.getDescription() +
+				"', '" + entity.getHomework() + "', '" + entity.getDiscipline() +
+				"', '" + entity.getTeacherLogin() + "', '" + entity.getGroupId() + "')";
+		ResultSet result = connector.executeStatement(statement);
+		try {
+			Integer id = result.getInt(0);
+			entity.setLessonId(id);
+			return entity;
+		} catch (SQLException exception) {
+			return null;
+		}
+	}
 
-    @Override
-    public Optional<Lesson> findById(Integer id) {
-        return Optional.empty();
-    }
+	@Override
+	public Optional<Lesson> findById(Integer id) {
+		String statement = "SELECT * FROM Users WHERE login = '" + id + "')";
+		ResultSet lessonsSet = connector.executeStatement(statement);
+		if (lessonsSet != null) {
+			try {
+				LocalDate localDate = lessonsSet.getObject(1, LocalDate.class);
+				LocalTime localTime = lessonsSet.getObject(2, LocalTime.class);
 
-    @Override
-    public Iterable<Lesson> findAll() {
-        String getLessonsCommand = "SELECT * FROM Lessons";
+				LocalDateTime dateTime = localDate.atTime(localTime);
 
-        ResultSet lessonsSet = connector.executeStatement(getLessonsCommand);
+				String homework = lessonsSet.getString(3);
+				String discipline = lessonsSet.getString(4);
+				Integer groupId = lessonsSet.getInt(5);
+				String description = lessonsSet.getString(6);
+				String teacherLogin = lessonsSet.getString(7);
 
-        ArrayList<Lesson> lessons = new ArrayList<>();
-        try {
-            while (lessonsSet.next()) {
-                Integer id = lessonsSet.getInt(0);
-                LocalDate localDate = lessonsSet.getObject(1, LocalDate.class);
-                LocalTime localTime = lessonsSet.getObject(2, LocalTime.class);
+				Map<String, Boolean> presentsForLesson = new HashMap<>();
+				Lesson lesson = new Lesson(presentsForLesson);
 
-                LocalDateTime dateTime = localDate.atTime(localTime);
+				String getPresentsCommand = String.format("Select * FROM Presents WHERE lesson_id = %s", id);
 
-                String homework = lessonsSet.getString(3);
-                String discipline = lessonsSet.getString(4);
-                Integer groupId = lessonsSet.getInt(5);
-                String description = lessonsSet.getString(6);
-                String teacherLogin = lessonsSet.getString(7);
+				ResultSet presentSet = connector.executeStatement(getPresentsCommand);
+				while (presentSet.next()) {
+					lesson.addPresent(presentSet.getString(1));
+				}
 
-                Map<String, Boolean> presentsForLesson = new HashMap<>();
-                Lesson lesson = new Lesson(id, dateTime, description, discipline, homework, groupId, teacherLogin, presentsForLesson);
+				lesson.setLessonId(id);
+				lesson.setDateTime(dateTime);
+				lesson.setDescription(description);
+				lesson.setHomework(homework);
+				lesson.setDiscipline(discipline);
+				lesson.setTeacherLogin(teacherLogin);
+				lesson.setGroupId(groupId);
 
-                String getPresentsCommand = String.format("Select * FROM Presents WHERE lesson_id = %s", id);
+				return Optional.of(lesson);
+			} catch (SQLException e) {
+				return Optional.empty();
+			}
+		}
+		else {
+			return Optional.empty();
+		}
+	}
 
-                ResultSet presentSet = connector.executeStatement(getPresentsCommand);
-                while (presentSet.next()) {
-                    lesson.addPresent(presentSet.getString(1));
-                }
+	@Override
+	public Iterable<Lesson> findAll() {
+		String getLessonsCommand = "SELECT * FROM Lessons";
 
-                lesson.setLessonId(id);
-                lesson.setDateTime(dateTime);
-                lesson.setDescription(description);
-                lesson.setHomework(homework);
-                lesson.setDiscipline(discipline);
-                lesson.setTeacherLogin(teacherLogin);
-                lesson.setGroupId(groupId);
+		ResultSet lessonsSet = connector.executeStatement(getLessonsCommand);
 
-                lessons.add(lesson);
-            }
+		ArrayList<Lesson> lessons = new ArrayList<>();
+		try {
+			while (lessonsSet.next()) {
+				Integer id = lessonsSet.getInt(0);
+				LocalDate localDate = lessonsSet.getObject(1, LocalDate.class);
+				LocalTime localTime = lessonsSet.getObject(2, LocalTime.class);
 
-            lessonsSet.close();
+				LocalDateTime dateTime = localDate.atTime(localTime);
 
-            return lessons;
-        } catch (SQLException ex) {
-            return null;
-        }
-    }
+				String homework = lessonsSet.getString(3);
+				String discipline = lessonsSet.getString(4);
+				Integer groupId = lessonsSet.getInt(5);
+				String description = lessonsSet.getString(6);
+				String teacherLogin = lessonsSet.getString(7);
 
-    @Override
-    public void deleteById(Integer id) {
-        String deleteLessonCommand = String.format("DELETE FROM Lessons WHERE id = %s", id);
+				Map<String, Boolean> presentsForLesson = new HashMap<>();
+				Lesson lesson = new Lesson(presentsForLesson);
 
-        ResultSet resultSet = connector.executeStatement(deleteLessonCommand);
-        try {
-            resultSet.close();
-        } catch (SQLException ignored) {}
-    }
+				String getPresentsCommand = String.format("Select * FROM Presents WHERE lesson_id = %s", id);
 
-    @Override
-    public void update(Lesson entity) {
-        try {
-        LocalDate date = entity.getDateTime().toLocalDate();
-        LocalTime time = entity.getDateTime().toLocalTime();
+				ResultSet presentSet = connector.executeStatement(getPresentsCommand);
+				while (presentSet.next()) {
+					lesson.addPresent(presentSet.getString(1));
+				}
 
-        Integer id = entity.getLessonId();
-        String homework = entity.getHomework();
-        String discipline = entity.getDiscipline();
-        Integer groupId = entity.getGroupId();
-        String description = entity.getDescription();
-        String teacherLogin = entity.getTeacherLogin();
+				lesson.setLessonId(id);
+				lesson.setDateTime(dateTime);
+				lesson.setDescription(description);
+				lesson.setHomework(homework);
+				lesson.setDiscipline(discipline);
+				lesson.setTeacherLogin(teacherLogin);
+				lesson.setGroupId(groupId);
 
-        String updateLessonCommand = String.format("UPDATE Lessons SET date = date '%s', time = time '%s', homework = '%s', discipline = '%s', group_id = %s, description = '%s', teacher_login = '%s' WHERE id = %s",
-                date, time, homework, discipline, groupId, description, teacherLogin, id);
+				lessons.add(lesson);
+			}
 
-        ResultSet resultSet = connector.executeStatement(updateLessonCommand);
-        resultSet.close();
+			lessonsSet.close();
 
-        Map<String, Boolean> presents = entity.getIsPresent();
+			return lessons;
+		} catch (SQLException ex) {
+			return null;
+		}
+	}
 
-        for (String name : presents.keySet()) {
-            if (presents.get(name)) {
-                String existsPresentCommand = String.format("EXISTS (SELECT * FROM Presents WHERE lesson_id = %s AND login = '%s')", id, name);
+	@Override
+	public void deleteById(Integer id) {
+		String deleteLessonCommand = String.format("DELETE FROM Lessons WHERE id = %s", id);
 
-                ResultSet present = connector.executeStatement(existsPresentCommand);
-                if (present == null) {
-                    String addPresentCommand = String.format("INSERT INTO Presents VALUES(%s, '%s')", id, name);
+		ResultSet resultSet = connector.executeStatement(deleteLessonCommand);
+		try {
+			resultSet.close();
+		} catch (SQLException ignored) {}
+	}
 
-                    ResultSet results = connector.executeStatement(addPresentCommand);
-                    results.close();
-                } else {
-                    present.close();
-                }
-            } else {
-                String existsPresentCommand = String.format("EXISTS (SELECT * FROM Presents WHERE lesson_id = %s AND login = '%s')", id, name);
+	@Override
+	public void update(Lesson entity) {
+		try {
+			LocalDate date = entity.getDateTime().toLocalDate();
+			LocalTime time = entity.getDateTime().toLocalTime();
 
-                ResultSet present = connector.executeStatement(existsPresentCommand);
-                if (present != null) {
-                    present.close();
+			Integer id = entity.getLessonId();
+			String homework = entity.getHomework();
+			String discipline = entity.getDiscipline();
+			Integer groupId = entity.getGroupId();
+			String description = entity.getDescription();
+			String teacherLogin = entity.getTeacherLogin();
 
-                    String deletePresentCommand = String.format("DELETE FROM Presents WHERE lesson_id = %s AND login = '%s'", id, name);
+			String updateLessonCommand = String.format("UPDATE Lessons SET date = date '%s', time = time '%s', homework = '%s', discipline = '%s', group_id = %s, description = '%s', teacher_login = '%s' WHERE id = %s",
+					date, time, homework, discipline, groupId, description, teacherLogin, id);
 
-                    ResultSet results = connector.executeStatement(deletePresentCommand);
-                    results.close();
-                }
-            }
-            }
-        } catch (SQLException ignored) {}
-    }
+			ResultSet resultSet = connector.executeStatement(updateLessonCommand);
+			resultSet.close();
 
-    @Override
-    public boolean existsById(Integer login) {
-        String existsLessonCommand = String.format("EXISTS (SELECT discipline FROM Lessons WHERE id = %s)", login);
+			Map<String, Boolean> presents = entity.getIsPresent();
 
-        ResultSet lesson = connector.executeStatement(existsLessonCommand);
-        return lesson != null;
-    }
+			for (String name : presents.keySet()) {
+				if (presents.get(name)) {
+					String existsPresentCommand = String.format("EXISTS (SELECT * FROM Presents WHERE lesson_id = %s AND login = '%s')", id, name);
+
+					ResultSet present = connector.executeStatement(existsPresentCommand);
+					if (present == null) {
+						String addPresentCommand = String.format("INSERT INTO Presents VALUES(%s, '%s')", id, name);
+
+						ResultSet results = connector.executeStatement(addPresentCommand);
+						results.close();
+					} else {
+						present.close();
+					}
+				} else {
+					String existsPresentCommand = String.format("EXISTS (SELECT * FROM Presents WHERE lesson_id = %s AND login = '%s')", id, name);
+
+					ResultSet present = connector.executeStatement(existsPresentCommand);
+					if (present != null) {
+						present.close();
+
+						String deletePresentCommand = String.format("DELETE FROM Presents WHERE lesson_id = %s AND login = '%s'", id, name);
+
+						ResultSet results = connector.executeStatement(deletePresentCommand);
+						results.close();
+					}
+				}
+			}
+		} catch (SQLException ignored) {}
+	}
+
+	@Override
+	public boolean existsById(Integer login) {
+		String existsLessonCommand = String.format("EXISTS (SELECT discipline FROM Lessons WHERE id = %s)", login);
+
+		ResultSet lesson = connector.executeStatement(existsLessonCommand);
+		return lesson != null;
+	}
 }
--- src/main/java/storage/postgresql/UserRepositoryImpl.java
+++ src/main/java/storage/postgresql/UserRepositoryImpl.java
@@ -3,62 +3,111 @@ package storage.postgresql;
 import domain.User;
 import domain.UserType;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import storage.UserRepository;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
 
 
+@Data
 @AllArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
 
-    private Connector connector;
-    private final HashMap<UserType, String> convertUserTypeForm = init();
+	private Connector connector;
+	private final HashMap<UserType, String> convertUserTypeForm = init();
 
-    @Override
-    public User saveNewEntity(User entity) {
-        return null;
-    }
+	@Override
+	public User saveNewEntity(User entity) {
+		String statement = "INSERT INTO Users VALUES('" + entity.getLogin() +
+				"', '" + entity.getName() + "', '" + entity.getUserType() +
+				"', '" + entity.getGroupId() + "', '" + entity.getPassword() + "')";
+		connector.executeStatement(statement);
+		ResultSet result = connector.executeStatement(statement);
+		try {
+			String login = result.getString(0);
+			entity.setLogin(login);
+			return entity;
+		} catch (SQLException exception) {
+			return null;
+		}
+	}
 
-    @Override
-    public Optional<User> findById(String id) {
-        return Optional.empty();
-    }
+	@Override
+	public Optional<User> findById(String login) {
+		String statement = "SELECT * FROM Users WHERE login = '" + login + "')";
+		ResultSet result = connector.executeStatement(statement);
+		if (result != null) {
+			try {
+				String name = result.getString(1);
+				UserType userType = result.getObject(2, UserType.class);
+				Integer groupId = result.getInt(3);
+				String password = result.getString(4);
+				User user = new User(login, password, name, userType, groupId);
+				return Optional.of(user);
+			} catch (SQLException e) {
+				return Optional.empty();
+			}
+		}
+		else {
+			return Optional.empty();
+		}
+	}
 
-    @Override
-    public Iterable<User> findAll() {
-        return null;
-    }
+	@Override
+	public Iterable<User> findAll() {
+		String statement = "SELECT * FROM Users";
+		ResultSet result = connector.executeStatement(statement);
+		ArrayList<User> userList = new ArrayList<>();
+		try {
+			while (result.next()) {
+				String login = result.getString(0);
+				String name = result.getString(1);
+				UserType userType = result.getObject(2, UserType.class);
+				Integer groupId = result.getInt(3);
+				String password = result.getString(4);
+				User user = new User(login, password, name, userType, groupId);
+				userList.add(user);
+			}
+			return userList;
+		} catch (SQLException exception) {
+			return null;
+		}
+	}
 
-    @Override
-    public void deleteById(String id) {
+	@Override
+	public void deleteById(String id) {
+		String statement = "DELETE FROM Users WHERE login = '" + id + "'";
+		connector.executeStatement(statement);
+	}
 
-    }
+	@Override
+	public void update(User entity) {
+		String newUserType = convertUserTypeForm.get(entity.getUserType());
+		String statement = "UPDATE Users SET " + "(name, group_id, user_type, password_salt, password_hash) = " +
+				"('" + entity.getName() + "', " + entity.getGroupId() + ", '" + newUserType +
+				"', '" + entity.getPassword() + "', '" + entity.getPassword() + "') " +
+				"WHERE login = '" + entity.getLogin() + "'";
+		connector.executeStatement(statement);
+	}
 
-    @Override
-    public void update(User entity) {
-        String newUserType = convertUserTypeForm.get(entity.getUserType());
-        String statement = "UPDATE Users SET " + "(name, group_id, user_type, password_salt, password_hash) = " +
-                "('" + entity.getName() + "', " + entity.getGroupId() + ", '" + newUserType +
-                "', '" + entity.getPassword() + "', '" + entity.getPassword() + "') " +
-                "WHERE login = '" + entity.getLogin() + "'";
-        connector.executeStatement(statement);
-    }
+	@Override
+	public boolean existsById(String login) {
+		String statement = "EXISTS (SELECT * FROM Users WHERE login = '"
+				+ login + "')";
+		ResultSet result = connector.executeStatement(statement);
+		return result != null;
+	}
 
-    @Override
-    public boolean existsById(String login) {
-        String statement = "SELECT * FROM Users WHERE login = '" + login + "'";
-        ResultSet result = connector.executeStatement(statement);
-        return result != null;
-    }
-
-    private HashMap<UserType, String> init() {
-        HashMap<UserType, String> convertHashMap = new HashMap<>();
-        convertHashMap.put(UserType.STUDENT, "student");
-        convertHashMap.put(UserType.GROUP_HEAD, "group_head");
-        convertHashMap.put(UserType.TEACHER, "teacher");
-        convertHashMap.put(UserType.ADMIN, "admin");
-        return convertHashMap;
-    }
+	private HashMap<UserType, String> init() {
+		HashMap<UserType, String> convertHashMap = new HashMap<>();
+		convertHashMap.put(UserType.STUDENT, "student");
+		convertHashMap.put(UserType.GROUP_HEAD, "group_head");
+		convertHashMap.put(UserType.TEACHER, "teacher");
+		convertHashMap.put(UserType.ADMIN, "admin");
+		return convertHashMap;
+	}
 }
