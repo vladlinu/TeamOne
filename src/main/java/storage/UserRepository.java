5f2ba6cfcde17a1e036ebd46fed6079eae43ecaf
a-mountain <frostnext2@gmail.com>
2020-12-14 14:06:12 +0200

    Remove redundant imports

--- src/main/java/storage/UserRepository.java
+++ src/main/java/storage/UserRepository.java
@@ -1,8 +1,6 @@
 package storage;
 
 import domain.User;
-import domain.UserType;
-import org.graalvm.compiler.lir.LIRInstruction;
 
 public interface UserRepository extends CrudRepository<User, String> {
 }
